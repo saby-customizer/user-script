4214ca82e6af9de136cf517aeaf61511290f3032
Igor Novozhilov <maildrakon@gmail.com>
2023-12-01 11:01:10 +0300

    cloud-holiday_new-year

--- christmas-tree.user.js
+++ christmas-tree.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          SABY Christmas - Tree
 // @namespace     saby-customizer
-// @version       2.0.7
+// @version       2.0.8
 // @author        IgorNovozhilov
 // @description   Персональная настройка saby приложений для решения повседневных задач, и не только...
 // @homepage      https://saby-customizer.github.io
@@ -25,7 +25,7 @@
     .NavigationPanels-Accordion__title_level-1::after, .NavigationPanels-Accordion__title_level-2_active::after {
       background: none !important;
     }
-    .cloud-newYearPage .NavigationPanels-Accordion__container:before {
+    .cloud-holiday_new-year .NavigationPanels-Accordion__container::before {
       background: none !important;
     }
     .NavigationPanels-Accordion__tree {
